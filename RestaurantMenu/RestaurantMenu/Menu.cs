1d28ba3c57e9d4e5cca9469f4987484fb4a5776b
dantehargrow <dantehargrow@gmail.com>
2020-10-12 23:02:40 -0500

    created repository

---
+++ RestaurantMenu/RestaurantMenu/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class Menu
+    {
+        public List<MenuItem> menu;
+        public DateTime lastUpdated;
+
+        public Menu()
+        {
+            
+        }
+    }
+}
---
+++ RestaurantMenu/RestaurantMenu/MenuItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class MenuItem
+    {
+        double price { get; set; }
+        string description { get; set; }
+        string category { get; set; }
+        bool isNew { get; set; }
+
+        public MenuItem(double price, string description,string category, bool isnew = true)
+        {
+            this.price = price;
+            this.description = description;
+            this.category = category;
+            this.isNew = isnew;
+        }
+    }
+}
